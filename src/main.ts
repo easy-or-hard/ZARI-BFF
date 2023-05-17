6e1c2865b26820ba6e527382a91ed71bed0ec084
이요 <krb7korea@me.com>
2023-05-17 18:04:04 +0900

    동적 포트로 변경

--- src/main.ts
+++ src/main.ts
@@ -50,7 +50,7 @@ async function bootstrap() {
   const prismaService: PrismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
 
-  await app.listen(3000);
+  await app.listen(configService.get('PORT'));
 }
 
 bootstrap();

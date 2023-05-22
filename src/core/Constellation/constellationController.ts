d98d6d9d43c9fb4e71f725ec5faa42fc6d92d45f
이요 <krb7korea@me.com>
2023-05-22 16:05:17 +0900

    출력 구조체 변경, swagger 설명 수정

--- src/core/Constellation/constellationController.ts
+++ src/core/Constellation/constellationController.ts
@@ -10,13 +10,18 @@ export class ConstellationController {
   constructor(private readonly constellationService: ConstellationService) {}
 
   @Get()
-  @ApiOperation({ summary: '별자리 찾기' })
+  @ApiOperation({ summary: '모든 별자리 찾기' })
   @ApiOkResponse({
-    description: '별자리를 찾았어요',
+    description: '모든 별자리를 찾았어요',
     type: ReadConstellationsResponseDto,
   })
-  findAll() {
-    return this.constellationService.findAll();
+  async findAll() {
+    const constellations = await this.constellationService.findAll();
+    return {
+      statusCode: 200,
+      message: '모든 별자리를 찾았어요',
+      data: constellations,
+    };
   }
 
   @Get(':IAU')
@@ -25,7 +30,12 @@ export class ConstellationController {
     description: '별자리를 찾았어요',
     type: ReadConstellationResponseDto,
   })
-  findOne(@Param('IAU') IAU: string) {
-    return this.constellationService.findOne(IAU);
+  async findOne(@Param('IAU') IAU: string) {
+    const constellation = await this.constellationService.findOne(IAU);
+    return {
+      statusCode: 200,
+      message: '별자리 하나 찾았어요',
+      data: constellation,
+    };
   }
 }

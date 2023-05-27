102634e6cd0f46ee631af43e194d188d540fde78
이요 <krb7korea@me.com>
2023-05-27 21:04:05 +0900

    사용하지 않는 임포트 삭제

--- src/core/Constellation/constellation.service.ts
+++ src/core/Constellation/constellation.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'nestjs-prisma';
 
 @Injectable()

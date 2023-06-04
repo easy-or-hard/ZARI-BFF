ced16752c004233c15a2ffd7317f0057a1ee187e
이요 <krb7korea@me.com>
2023-06-04 15:46:20 +0900

    정상응답을 이제는 엔티티로 돌려주기때문에 필요 없는 dto 삭제

--- src/core/byeol/dto/response/update-byeol-response.dto.ts
+++
@@ -1,9 +0,0 @@
-import { BaseResponseDto } from '../../../../lib/common/dto/response.dto';
-import { ApiProperty } from '@nestjs/swagger';
-
-export default class UpdateByeolResponseDto implements BaseResponseDto {
-  @ApiProperty({ example: 200 })
-  statusCode: number;
-  @ApiProperty({ example: '별 이름을 바꿨어요' })
-  message: string;
-}
